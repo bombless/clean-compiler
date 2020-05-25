5ab43e275f057b070e4a11d25ebe64dc2f0209d5
John van Groningen <johnvg@cs.ru.nl>
2020-05-25 17:55:35 +0200

    remove include <windows.h> from windows_io.c

--- backendC/CleanCompilerSources/windows_io.c
+++ backendC/CleanCompilerSources/windows_io.c
@@ -7,19 +7,6 @@
 #include "system.h"
 #include <stdio.h>
 
-#ifdef _WIN64
-# undef _WINDOWS_
-# include <windows.h>
-#else
-# ifdef __MWERKS__
-#	include <x86_prefix.h>
-# else
-#	define _X86_
-# endif
-# include <windef.h>
-# include <winbase.h>
-#endif
-
 File FOpen (char *fname,char *mode)
 {
 	return fopen (fname,mode);
