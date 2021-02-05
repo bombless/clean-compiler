4fb1c973a13b62b016e65dfdd3586526360b00f6
John <johnvg@cs.ru.nl>
2021-02-05 15:05:46 +0100

    use ArgEnv instead of ../libraries/ArgEnvUnix in make.linux_arm.sh

--- unix/make.linux_arm.sh
+++ unix/make.linux_arm.sh
@@ -6,6 +6,6 @@ CLM=clm
 (cd main/Unix; make -f Makefile.arm all);
 $CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
 $CLM -h 32M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
-	-I ../libraries/ArgEnvUnix \
+	-IL ArgEnv \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
