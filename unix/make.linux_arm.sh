52f80871080df195f9896c75bb990f4fe368e1cc
ARM64 Builder <clean@cs.ru.nl>
2021-02-10 16:29:18 +0000

    in Makefile.arm remove make of ArgEnvC.o in libraries/ArgEnvUnix

--- unix/make.linux_arm.sh
+++ unix/make.linux_arm.sh
@@ -2,9 +2,8 @@
 CLM=clm
 
 (cd backendC/CleanCompilerSources; make -f Makefile.arm)
-(cd ../libraries/ArgEnvUnix; make -f Makefile_arm ArgEnvC.o)
 (cd main/Unix; make -f Makefile.arm all);
-$CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
+$CLM -ci -fusion -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
 $CLM -h 32M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-IL ArgEnv \
 	-l backendC/CleanCompilerSources/backend.a \
