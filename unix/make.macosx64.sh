5762c2596f23085e24b61c47199fb18e6951fb81
John <johnvg@cs.ru.nl>
2021-06-28 13:10:45 +0200

    bug fix for macosx, in make.macosx64.sh set the stack size to 2M, make the heap size the same as for 64 bit linux (60M instead of 48M), compile module backendconvert with -fusion

--- unix/make.macosx64.sh
+++ unix/make.macosx64.sh
@@ -5,6 +5,8 @@ CLM=clm
 
 (cd backendC/CleanCompilerSources; make -f Makefile.linux64)
 (cd main/Unix; make -f Makefile all);
-$CLM -gcm -h 48M -nt -nw -ci -nr -IL ArgEnv -I backend -I frontend -I main -I main/Unix \
+$CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
+$CLM -gcm -h 60M -s 2M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
+	-IL ArgEnv \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
