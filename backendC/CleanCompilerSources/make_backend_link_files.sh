cebfb8dbeaf076e9c5c6a6fd95bbe5fa743e26ef
John <johnvg@cs.ru.nl>
2022-02-18 12:27:31 +0100

    add BESafeNormalNode to backend_library, add creating backend_library in make_backend_link_files.sh

--- backendC/CleanCompilerSources/make_backend_link_files.sh
+++ backendC/CleanCompilerSources/make_backend_link_files.sh
@@ -1,2 +1,4 @@
-grep 'Clean (BE' backend.h | sed -e 's\Clean (\/EXPORT: \' -e 's/\(BE[0-9a-zA-Z_]*\).*/\1/' -e 's/$/\r/' > backend.link
-grep 'Clean (BE' backend.h | sed -e 's\Clean (\/EXPORT:\'  -e 's/\(BE[0-9a-zA-Z_]*\).*/\1/' -e 's/$/\r/' > backend.link64
+#!/bin/bash
+grep 'Clean (BE' backend.h | sed -e 's$Clean (\(BE[0-9a-zA-Z_]*\).*$/EXPORT: \1$' -e 's/$/\r/' > backend.link
+grep 'Clean (BE' backend.h | sed -e 's$Clean (\(BE[0-9a-zA-Z_]*\).*$/EXPORT:\1$' -e 's/$/\r/' > backend.link64
+grep 'Clean (BE' backend.h | sed -e 's/\Clean (\(BE[0-9a-zA-Z_]*\).*/\1/' -e 's/$/\r/' -e '1ibackend.dll\r' > backend_library
